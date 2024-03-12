6794e36a6003238a165d05043b48a937f6bb0b4b
Vrain Jean-Baptiste <j-b.vrain@outlook.fr>
2024-03-12 05:54:50 +0100

    [INIT] init view engine

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res) => {
+  // demande de rendu EJS
+  res.render("pages/home", { user: { name: "Jean-Baptiste" } }); // on donne le chemin dans views, et on omet le .ejs
+});
+
+module.exports = app;
